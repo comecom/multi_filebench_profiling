63cb51fbfd381f101be596777ec45ead799eed01
comecom <immerhjw@gmail.com>
2021-02-16 16:31:55 -0800

    multi app profiling

--- README.md
+++
@@ -1,7 +0,0 @@
-# shared_pcm_fxmark
-
-> make for pcm_jw and fxmark respectively
-
-> need msr module for pcm execution
-
-> cores for fxmark [1, 2, 4, 10]
--- cpu_util.c
+++ cpu_util.c
@@ -1,39 +1,94 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<linux/sched.h>
+#include<linux/kernel.h>
 
-#define num_node 1
-#define core_per_node 20
+#define num_node 8
+#define core_per_node 15
 
 int past[121][4]={0, }, present[121][4]={0, };
 int diff[121][4], total[121];
 
+void get_cpu_util_csv(){
+        int n_core = num_node * core_per_node;
+        int n4, n5, n6, n7, n8, n9;
+
+        FILE* statFile = fopen("/proc/stat", "r");
+	FILE* resFile = fopen("util.log", "a");
+        char cpuId[6]={0};
+
+        memset(total, 0, sizeof(total));
+
+        for(int i=0;i<=n_core;i++){
+                fscanf(statFile, "%s %d %d %d %d %d %d %d %d %d %d",
+                                cpuId, &present[i][0],  &present[i][1], &present[i][2], &present[i][3],
+                                &n4, &n5, &n6, &n7, &n8, &n9);
+
+                for(int j=0;j<4;j++){
+                        diff[i][j] = present[i][j] - past[i][j];
+                        total[i] += diff[i][j];
+                }
+                memcpy(past[i], present[i], sizeof(int)*4);
+        }
+        
+	//	utilization per NODE
+        double t, idle;
+
+	//	real cpu utilization value
+        for(int i=0;i<n_core;i++){
+                if(i%core_per_node==0){
+                        t = 0;
+                        idle = 0;
+                }
+                t+=(double)total[i+1];
+                idle+=(double)diff[i+1][3];
+
+                if(i%core_per_node==core_per_node-1){
+                        //printf("%lf %lf\n", idle, t);
+                        printf("%lf\t", 100.0*(1.0-idle/t));
+                       	fprintf(resFile, "%lf\t", 100.0*(1.0-idle/t));
+                }
+        }
+	printf("\n");
+	fprintf(resFile, "\n");
+        
+	fclose(statFile);
+	fclose(resFile);
+
+        return ;
+}
+
 void get_cpu_util(){
 	int n_core = num_node * core_per_node;
 	int n4, n5, n6, n7, n8, n9;
-	
+
 	FILE* statFile = fopen("/proc/stat", "r");
 	char cpuId[6]={0};
 
 	memset(total, 0, sizeof(total));
-	
+
 	for(int i=0;i<=n_core;i++){
 		fscanf(statFile, "%s %d %d %d %d %d %d %d %d %d %d",
-				cpuId, &present[i][0],  &present[i][1], &present[i][2], &present[i][3], 
+				cpuId, &present[i][0],  &present[i][1], &present[i][2], &present[i][3],
 				&n4, &n5, &n6, &n7, &n8, &n9);
 		//printf("%s\n", cpuId);
-	
+
 		for(int j=0;j<4;j++){
 			diff[i][j] = present[i][j] - past[i][j];
 			total[i] += diff[i][j];
 		}
 		//printf("diff : %d, total : %d\n", diff[i][3], total[i]);
-		printf("cpu%d usage : %f%%\n", i, 100.0*(1.0-(diff[i][3]/(double)total[i])));
-		
+		//printf("cpu%d usage : %f%%\n", i, 100.0*(1.0-(diff[i][3]/(double)total[i])));
+
 		memcpy(past[i], present[i], sizeof(int)*4);
 	}
-
 	//utilization per NODE
 	double t, idle;
 
@@ -54,13 +109,34 @@ void get_cpu_util(){
 
 	return ;
 }
-int main(void){
-	
-	while(1){
-		get_cpu_util();
 
-		sleep(1);
+int main(int argc, char *argv[])
+{
+	int time = 0;
+	if(argc == 1){
+		while(time<60){
+			get_cpu_util();
+			time++;
+			sleep(1);
+		}
 	}
+	else if(argc == 2 && strcmp(argv[1], "-csv")==0){
+		FILE* resFile = fopen("util.log", "w");
 
+		for(int i=0;i<num_node;i++){
+			fprintf(resFile, "NODE #%d\t", i);
+                	printf("NODE #%d\t\t", i);
+		}
+		fprintf(resFile, "\n");
+                printf("\n");
+		fclose(resFile);
+
+		while(time<63){
+			get_cpu_util_csv();
+			time++;
+			sleep(1);
+		}
+	}
 	return 0;
 }
+
